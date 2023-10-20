b1d101a032cd9cd0557e14bb5783af89cc7f8807
Itachi-789 <vighneshzagade4@gmail.com>
2023-10-21 00:12:32 +0530

    Created Pages and added Routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,35 @@
+import { Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import { LoginPage } from "./pages/LoginPage";
+import ProfilePage from "./pages/ProfilePage";
+import RegisterPage from "./pages/RegisterPage";
+import ResetPassword from "./pages/ResetPassword";
+import { useSelector } from "react-redux";
+
+function Layout() {
+  const user = useSelector((state)=> state.user); 
+  const location = useLocation();
+  console.log(user)
+  return user?.token ? (
+    <Outlet/>
+  ) : (
+    <Navigate to="/login" state={{from: location}} replace/>
+  )
+}
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="text-3xl font-bold underline">
+      <Routes>
+        <Route element={<Layout/>}>
+          <Route path="/" element={<Homepage/>}/>
+          <Route path="/profile/:id" element={<ProfilePage/>}/>
+        </Route>
+        <Route path="/register" element={<RegisterPage/>}/>
+        <Route path="/login" element={<LoginPage/>}/>
+        <Route path="/reset-password" element={<ResetPassword/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 import { store } from "./Redux/store.js";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
